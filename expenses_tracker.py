d0db780aba2c96f3e2aa91878f4285fe3dec4162
Lakony34 <nicolas.demars@epfedu.fr>
2026-02-05 11:51:39 +0100

    Python v1 commit

--- expenses_tracker.py
+++ expenses_tracker.py
@@ -1,2 +1,21 @@
 # Voici le code à réaliser pour le projet de suivi des dépenses personnelles en Python. Ce code permet de suivre les dépenses, de les catégoriser.
+list_expenses_Food =[]
+list_expenses_Divertissement =[]
+list_expenses_Taxes =[]
+user_choice = input(str("Dans quelle catégorie souhaitez-vous enregistrer votre dépense ? (F : Food | D : Divertissement | T : Taxes) : "))
 
+if user_choice == "F":
+    user_price_F = input(int("Combien d'€ est votre dépense ? :"))
+    list_expenses_Food.append(user_price_F)
+
+elif user_choice == "D":
+    user_price_D = input(int("Combien d'€ est votre dépense ? :"))
+    list_expenses_Food.append(user_price_D)
+
+elif user_choice == "T":
+    user_price_T = input(int("Combien d'€ est votre dépense ? :"))
+    list_expenses_Food.append(user_price_T)
+else:
+    print("Error")
+
+print("Voici à présent vos dépenses actuelles: \n", sum(list_expenses_Food) + sum(list_expenses_Divertissement) + sum(list_expenses_Taxes))
