bc6bd0c4bc0d607d4ff8e89d0ea054aaff3a05f7
Luka-Rondal <luka.rondal@epfedu.fr>
2026-02-05 12:22:10 +0100

    Add frame to create different pages (structure change)

--- vote_interface/vote_interface.py
+++ vote_interface/vote_interface.py
@@ -1,44 +1,99 @@
+##Base from https://www.geeksforgeeks.org/python/tkinter-application-to-switch-between-different-page-frames/
+
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk
 from PIL import Image, ImageTk
 
+LARGEFONT = ("Verdana", 35)
 
 
-interface = 0
-def change_interface(n):
-    global interface
-    interface = n
+class tkinterApp(tk.Tk):
+    # __init__ function for class tkinterApp
+    def __init__(self, *args, **kwargs):
+
+        # __init__ function for class Tk
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.geometry("650x1155")
+        self.title("Vote for your favorite dish")
+        # Background
+        img = Image.open("background.jpg")
+        self.bg_img = ImageTk.PhotoImage(img)
+
+        bg_label = tk.Label(self, image=self.bg_img)
+        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+        # creating a container
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        # initializing frames to an empty array
+        self.frames = {}
+
+        # iterating through a tuple consisting
+        # of the different page layouts
+        for F in (StartPage, Page1):
+            frame = F(container, self)
+
+            # initializing frame of that object from
+            # startpage, page1, page2 respectively with
+            # for loop
+            self.frames[F] = frame
+
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    # to display the current frame passed as
+    # parameter
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
 
 
-def main():
-    root = tk.Tk()
-    root.title("Vote for your favorite food")
-    root.geometry("650x1155")
-    
-    
-    img = Image.open(r"C:\Users\15691\epf-programming\python projet\engineering-sandbox\vote_interface\background.jpg")
-    bg_img = ImageTk.PhotoImage(img)
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        img = Image.open("background.jpg")
+        self.bg_img = ImageTk.PhotoImage(img)
 
-    root.bg_img = bg_img  
-    bg_label = tk.Label(root, image=bg_img)
-    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        bg_label = tk.Label(self, image=self.bg_img)
+        bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+        # label of frame Layout 2
+        label = ttk.Label(self, text="Startpage", font=LARGEFONT)
 
+        # putting the grid in its place by using
+        # grid
+        label.grid(row=0, column=4, padx=10, pady=10)
 
-    # header
-    header = tk.Frame(root, bg="#111827", padx=18, pady=14)
-    header.pack(fill="x")
+        button1 = ttk.Button(
+            self, text="Vote", command=lambda: controller.show_frame(Page1)
+        )
 
-    if interface == 0:
-        vote_button = ttk.Button(root, text="Vote", command=change_interface(1))
-        result_button = ttk.Button(root, text="Results", command=change_interface(2))
-        winner_button = ttk.Button(root, text="Winner", command=change_interface(3))
-
-        vote_button.pack(ipadx=5, ipady=5, expand=True)
-        result_button.pack(ipadx=5, ipady=5, expand=True)
-        winner_button.pack(ipadx=5, ipady=5, expand=True)
-
-    root.mainloop()
+        # putting the button in its place by
+        # using grid
+        button1.grid(row=1, column=1, padx=10, pady=10)
 
 
-if __name__ == "__main__":
-    main()
+# second window frame page1
+class Page1(tk.Frame):
+    def __init__(self, parent, controller):
+
+        tk.Frame.__init__(self, parent)
+
+        # button to show frame 2 with text
+        # layout2
+        button1 = ttk.Button(
+            self, text="StartPage", command=lambda: controller.show_frame(StartPage)
+        )
+
+        # putting the button in its place
+        # by using grid
+        button1.grid(row=1, column=1, padx=10, pady=10)
+
+
+# Driver Code
+app = tkinterApp()
+app.mainloop()
