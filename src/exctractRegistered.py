9bc71fde2ea0330abd3c6017387737b8398a3d67
Leopold Stiffel <leopold.stiffel@epfedu.fr>
2026-02-06 15:58:55 +0100

    This project now has a main.py file with a basic frontend able to call extractRegistered

--- src/exctractRegistered.py
+++ src/exctractRegistered.py
@@ -13,31 +13,35 @@ with open(DATA_FILE, "r", encoding="utf-8") as f:
     data = json.load(f)
 
 
-listExpired = []
-listNotExpired = []
+def PrintItems():
+    # Getting the data part
+    listExpired = []
+    listNotExpired = []
 
-for item in data:
-    expiration_date = datetime.strptime(item["date"], "%d/%m/%Y")
-    expired = expiration_date < datetime.now()
-    if expired:
-        listExpired.append(item)
-    else:
-        listNotExpired.append(item)
-        distanceFromExpiration = (expiration_date.date() - date.today()).days
-        item.update({"days_until_expiration": distanceFromExpiration})
+    for item in data:
+        expiration_date = datetime.strptime(item["date"], "%d/%m/%Y")
+        expired = expiration_date < datetime.now()
+        if expired:
+            listExpired.append(item)
+        else:
+            listNotExpired.append(item)
+            distanceFromExpiration = (expiration_date.date() - date.today()).days
+            item.update({"days_until_expiration": distanceFromExpiration})
 
-listNotExpired.sort(key=lambda x: x["days_until_expiration"])
+    listNotExpired.sort(key=lambda x: x["days_until_expiration"])
 
-print("Expired items:")
-for expired_item in listExpired:
-    print("Name:", expired_item["name"], "| Expiration Date:", expired_item["date"])
-print("\nNot expired items:")
-for not_expired_item in listNotExpired:
-    print(
-        "Name:",
-        not_expired_item["name"],
-        "| Expiration Date:",
-        not_expired_item["date"],
-        "| Days until expiration:",
-        not_expired_item["days_until_expiration"],
-    )
+    # Printing the results
+    print("\nExpired items:")
+    for expired_item in listExpired:
+        print("Name:", expired_item["name"], "| Expiration Date:", expired_item["date"])
+    print("\nNot expired items:")
+    for not_expired_item in listNotExpired:
+        print(
+            "Name:",
+            not_expired_item["name"],
+            "| Expiration Date:",
+            not_expired_item["date"],
+            "| Days until expiration:",
+            not_expired_item["days_until_expiration"],
+        )
+    print("\n")
