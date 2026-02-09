fc35f26e193a0bac907d23158866ccb1dcf34905
martindmv <martin.demerdjiev@efpedu.fr>
2026-02-09 17:11:11 +0100

    added nutriscoreAnalyze function

--- main.py
+++ main.py
@@ -7,11 +7,27 @@ api = openfoodfacts.API(user_agent="MySchoolProject/1.0 (demerdjievm99@mail.com)
 
 
 def main():
-    code = input("Enter the product code: ")
-    product = getProduct(code)
+    while True:
+        code = input("Enter the product code: ")
 
-    # type of product is dict
-    print(product)
+        product = getProduct(code)
+
+        nutriscoreAnalyze(product)
+
+        # type of product is dict
+        print(product)
+
+
+def nutriscoreAnalyze(product):
+    nutriscore = product["nutrition_grades"]
+    if nutriscore in ["a", "b"]:
+        print("\n Safe to consume :)\n")
+    elif nutriscore in ["c"]:
+        print("\n To be consumed with moderation\n")
+    elif nutriscore in ["d", "e"]:
+        print("\n NOT SAFE\n")
+    else:
+        print(" Nutriscore not applicable ")
 
 
 def getProduct(code):
