65b303899dd869d28b8eef580a6ed8027377d97b
martindmv <martin.demerdjiev@efpedu.fr>
2026-02-09 15:39:16 +0100

    Created main() function

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ import openfoodfacts
 
 api = openfoodfacts.API(user_agent="MySchoolProject/1.0 (demerdjievm99@mail.com)")
 
-code = "3350033976269"
-print(api.product.get(code, fields=["code", "product_name", "nutrition_grades"]))
+
+def main():
+    code = input("Enter the product code: ")
+    product = api.product.get(code, fields=["code", "product_name", "nutrition_grades"])
+    print(type(product))
