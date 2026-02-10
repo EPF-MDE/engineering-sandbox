3f6ef4c1fc543c463ba7eb80ebf674d178083567
EmileZ10 <emile.zanna@epfedu.fr>
2026-02-10 12:54:08 +0100

    lastest version after presentation in class

--- execution.py
+++ execution.py
@@ -2,7 +2,8 @@ from calculator import *
 from levelUp import *
 from user_id_function import *
 
+
 def main():
-    user_info()
+    name, surname, age, gender, weight, height, goal, activity_level = user_info()
     calories = levelUp_calculator(weight, height, age, activity_level, goal)
     print(f"\nYour daily calorie needs to {goal} weight is: {calories:.2f} calories.")
